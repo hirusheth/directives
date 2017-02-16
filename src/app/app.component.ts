a093ff38a2c98b45ab6aa26a2399faeb6703f825
Sandeep patel <sandy20011987@gmail.com>
2017-02-16 17:43:56 +0530

    custom one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'dir-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'dir-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dir works!';
 }
